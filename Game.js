f293d2156131dc49551782f29181f736277e047a
Tyrone Williams <Tyrone Williams>
2017-01-26 21:54:44 -0500

    Removed debug logs and WebGL

--- Game.js
+++ Game.js
@@ -6,30 +6,24 @@ function Game(gameMode, aiLevel, first, vsai1, vsai2){
   this.player1AI = vsai1;
   this.player2AI = vsai2;
 
-  //For WebGL Drawing
-  this.boardVertices = [];
-  this.boardColors = [];
   this.gameDone = false;
   
-
-  //Create ai objects for player
-  if(vsai1){
+  if(vsai1){ // Sets player 1 as AI
     this.ai1 = new AI(aiLevel, gameMode, 1);
   }
-  if(vsai2){ // If there is an ai player, create the object
+  if(vsai2){ // Sets player 2 as AI
     this.ai2 = new AI(aiLevel, gameMode, 2);
   }
-  if(gameMode == 2){ // Use for ultimate mode to keep track single board wins
-    this.boardsWon = [0,0,0,0,0,0,0,0,0];
+  if(gameMode == 2){
+    this.boardsWon = [0,0,0,0,0,0,0,0,0]; // Keep track of what single boards are won
     this.lastMove = null;
   }
 
   //Makes sure methods are not declared multiply times.
   if(typeof this.startGame != "function"){
-    console.log("Adding functions");
 
     /*
-     * Starts game, draws game board
+     * Sets the backend copy of the board and draws the game board
      */
     Game.prototype.startGame = function(){
 
@@ -80,7 +74,6 @@ function Game(gameMode, aiLevel, first, vsai1, vsai2){
      */
     Game.prototype.setbackEnd = function(){
       if(this.gameMode == 1){
-        console.log("Setting up for Normal Mode.");
         this.board =
         [
           [0, 0, 0],
@@ -88,7 +81,6 @@ function Game(gameMode, aiLevel, first, vsai1, vsai2){
           [0, 0, 0]
         ];
       }else if(this.gameMode == 2){
-        console.log("Setting up for Ultimate Mode.");
         this.board =
         [
           [ [[0, 0, 0], [0, 0, 0], [0, 0, 0]], // 0
@@ -237,7 +229,6 @@ function Game(gameMode, aiLevel, first, vsai1, vsai2){
      */
     Game.prototype.setPieceToPlayer = function(outerX, outerY, innerX, innerY){
       if(this.gameMode == 1){
-        console.log(this.board);
         //Check for valid move
         if(this.board[innerX][innerY] != 0){
           console.log("Invalid move.");
@@ -247,10 +238,7 @@ function Game(gameMode, aiLevel, first, vsai1, vsai2){
         this.board[innerX][innerY] = this.currentPlayer;
 
         if(this.gameOver()){ //Check if game is over
-          if(this.gl){
-            this.drawBoard();
-          }
-
+          
           this.gameDone = true;
           console.log("Game over. Player " + this.currentPlayer + " wins!");
           this.endScene();
@@ -263,35 +251,29 @@ function Game(gameMode, aiLevel, first, vsai1, vsai2){
         //If the next player is an AI, then get their move
         if(this.currentPlayer == 1 && this.player1AI){
           var move = this.ai1.getMove(this.board, null, null);
-          if(!this.gl){
-            //Perform click for AI only for SVG version
-            var elem = document.getElementById("00" + move.innerX + "" + move.innerY);
-            elem.setAttributeNS(null, 'class', 'player' + this.currentPlayer);
-          }
+          
+          //Perform click for AI only for SVG version
+          var elem = document.getElementById("00" + move.innerX + "" + move.innerY);
+          elem.setAttributeNS(null, 'class', 'player' + this.currentPlayer);
+        
           this.setPieceToPlayer(0, 0, move.innerX, move.innerY);
 
         }else if(this.currentPlayer == 2 && this.player2AI){
           var move = this.ai2.getMove(this.board, null, null);
-          if(!this.gl){
-            //Perform click for AI only for SVG version
-            var elem = document.getElementById("00" + move.innerX + "" + move.innerY);
-            elem.setAttributeNS(null, 'class', 'player' + this.currentPlayer);
-          }
+
+          //Perform click for AI only for SVG version
+          var elem = document.getElementById("00" + move.innerX + "" + move.innerY);
+          elem.setAttributeNS(null, 'class', 'player' + this.currentPlayer);
+          
           this.setPieceToPlayer(0, 0, move.innerX, move.innerY);
-        }else{
-          //If using opengl
-          if(this.gl){
-            this.drawBoard();
-          }
         }
       }else if(this.gameMode == 2){
         //Check for invalid move
         if(this.board[outerX][outerY][innerX][innerY] != 0 || this.boardsWon[(outerX*3) + outerY] != 0 || (this.lastMove && (outerX != this.lastMove.innerX || outerY != this.lastMove.innerY))){ // Check for valid move
-          if(!this.gl){
-            //Reset puice
-            var elem = document.getElementById(outerX + "" + outerY + "" + innerX + "" + innerY);
-            elem.setAttributeNS(null, 'class', 'piece');
-          }
+          
+          //Reset piece
+          var elem = document.getElementById(outerX + "" + outerY + "" + innerX + "" + innerY);
+          elem.setAttributeNS(null, 'class', 'piece');
 
           console.log("invalid move!");
           return;
@@ -302,17 +284,11 @@ function Game(gameMode, aiLevel, first, vsai1, vsai2){
         //If single is won, mark it currentPlayer, if tied, mark it -1
         if(this.boardWon(outerX, outerY)){
           this.boardsWon[(outerX*3) + outerY] = this.currentPlayer;
-          console.log("Single board won.");
-          console.log(this.boardsWon);
         }else if(this.boardTied(outerX, outerY)){
           this.boardsWon[outerX*3 + outerY] = -1;
-          console.log(this.boardsWon);
         }
 
         if(this.gameOver()){ // Check if game is over
-          if(this.gl){ // Check if using OpenGL
-            this.drawBoard();
-          }
           this.gameDone = true;
           console.log("Game Over. Player "+ this.currentPlayer + " wins!");
           this.endScene();
@@ -325,7 +301,6 @@ function Game(gameMode, aiLevel, first, vsai1, vsai2){
           innerX = -1;
           innerY = -1;
         }else{
-          console.log(innerX*3 + innerY);
           this.lastMove = {outerX: outerX, outerY: outerY, innerX: innerX, innerY: innerY};
         }
 
@@ -335,26 +310,20 @@ function Game(gameMode, aiLevel, first, vsai1, vsai2){
         //If the next player is AI, then get their move, else set up next player's move.
         if(this.currentPlayer == 1 && this.player1AI){
           var move = this.ai1.getMove(this.board, innerX, innerY);
-          if(!this.gl){
-            //Perform click for AI only for SVG version
-            var elem = document.getElementById("" + move.outerX + "" + move.outerY + "" + move.innerX + "" + move.innerY);
-            elem.setAttributeNS(null, 'class', 'player' + this.currentPlayer);
-          }
+          
+          //Perform click for AI only for SVG version
+          var elem = document.getElementById("" + move.outerX + "" + move.outerY + "" + move.innerX + "" + move.innerY);
+          elem.setAttributeNS(null, 'class', 'player' + this.currentPlayer);
+          
           this.setPieceToPlayer(move.outerX, move.outerY, move.innerX, move.innerY);
         }else if(this.currentPlayer == 2 && this.player2AI){
           var move = this.ai2.getMove(this.board, innerX, innerY);
-          if(!this.gl){
-            //Perform click for AI only for SVG version
-            var elem = document.getElementById("" + move.outerX + "" + move.outerY + "" + move.innerX + "" + move.innerY);
-            elem.setAttributeNS(null, 'class', 'player' + this.currentPlayer);
-          }
+
+          //Perform click for AI only for SVG version
+          var elem = document.getElementById("" + move.outerX + "" + move.outerY + "" + move.innerX + "" + move.innerY);
+          elem.setAttributeNS(null, 'class', 'player' + this.currentPlayer);
+
           this.setPieceToPlayer(move.outerX, move.outerY, move.innerX, move.innerY);
-        }else{
-          //Set up next player's move
-          if(this.gl){
-            this.drawBoard();
-          }
-          console.log(this.board);
         }
       }
     };
@@ -480,7 +449,8 @@ function Game(gameMode, aiLevel, first, vsai1, vsai2){
     };
 
     /*
-     * 
+     * Checks if game is won for any game mode. Doesn't not account for ties.
+     * return Returns true if game is won, false otherwise.
      */
     Game.prototype.gameWon = function(){
       if(this.gameMode == 1){
@@ -585,7 +555,6 @@ function Game(gameMode, aiLevel, first, vsai1, vsai2){
           for(var i = 0; i < 3; i++){
             for(var j = 0; j < 3; j++){
               if(this.boardsWon[(i*3) + j] == 0){
-                console.log(i,j);
                 return false;
               }
             }
@@ -605,7 +574,6 @@ function Game(gameMode, aiLevel, first, vsai1, vsai2){
     /*
      * Shows winner of the game or tied using SVG or WebGL
      * SVG draws a box over the game board
-     * WebGL, draws a line through the winning position
      * Adds a button on the bottom for restarting the game
      */
     Game.prototype.endScene = function(){
